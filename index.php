afa589173dca31a90f47a776f297dc319d17741e
m1czi <43167167+m1czi@users.noreply.github.com>
2020-11-05 13:34:12 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -27,22 +27,23 @@
        <div class="c">
             
         </div>
-
-      
-        
-        
+                     
     </div>
     <div class="left">
     <?php
     $conn = new mysqli("remotemysql.com", "scKdXHDy41", "HT27TrSRyB", "scKdXHDy41");
     $result = $conn -> query("select `scKdXHDy41`.`lib_autor_tytul`.`id_autor_tytul` AS `id_autor_tytul`,`scKdXHDy41`.`lib_autor`.`imie` AS `imie`,`scKdXHDy41`.`lib_tytul`.`tytul` AS `tytul` from `scKdXHDy41`.`lib_tytul` join `scKdXHDy41`.`lib_autor_tytul` join `scKdXHDy41`.`lib_autor` where `scKdXHDy41`.`lib_autor_tytul`.`id_autor` = `scKdXHDy41`.`lib_autor`.`id_autor` and `scKdXHDy41`.`lib_autor_tytul`.`id_tytul` = `scKdXHDy41`.`lib_tytul`.`id_tytul`");
     echo("<table border='1'>");
+    echo("<th>Id</th>");
+    echo("<th>Autor</th>");
+    echo("<th>Tytuł</th>");
+    echo("<th>Delete</th>");
     while($row = $result -> fetch_assoc()){
         echo("<tr class='myrow'>");
         echo("<td>".$row['id_autor_tytul']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['tytul']."</td>"."<td>
         <form action='delete.php' method='POST'>
         <input type='hidden' name='id' value=".$row['id_autor_tytul'].">
-        <input type='submit' value='delete'>
+        <input type='submit' class='del' value='Delete'>
         </form> </td>");
         echo("</tr>");
     }
@@ -55,7 +56,7 @@
         Autor <br>
             <input type="text" name="imie"><br>Tytuł <br>
             <input type="text" name="tytul"><br> 
-            <input type="submit" value="Wyślij">
+            <input type="submit" class='send' value="  Wyślij  ">
             
         </form>
     </div>
